e88b9c61c0f48f96955809d94c4d0fb2f5e595bb
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-23 16:06:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let driver = {};
 function updateDriverWithKeyAndValue(obj, key, value){
   let newObj = (...obj);
-  newObj.key = value
+  newObj.key = value;
   return Object.assign({}, newObj);
 };
